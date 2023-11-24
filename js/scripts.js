8cd055ae18561b0b4566daa69ac2ce2d35e6d2af
anyazeng <anya.tseng@gmail.com>
2023-11-24 12:03:54 +1100

    contentful updates

--- js/scripts.js
+++ js/scripts.js
@@ -214,3 +214,15 @@ function setLoading(isLoading) {
     document.querySelector("#button-text").classList.remove("hidden");
   }
 }
+
+//Contentful
+var client = contentful.createClient({
+    accessToken: '8c5Xf5iTeNDwSXNZTlTLFER4FuwvhyAhQaCQpY-YuAY',
+    space: 'ybkvnl96zwpj'
+  })
+  // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
+  client
+    .getEntry('5aCQoPlgjap7Tij4f6HytB')
+    .getEntry('3aD8BObK2dVyeRI7u5gmr')
+    .then((entry) => console.log(entry))
+    .catch((err) => console.log(err))
